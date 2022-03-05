28dfa1a760cdf55176d31761ad8d5690b3309f8d
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-03-05 18:23:48 -0500

    using boiler plate, working out from lesson, building adjusting, still very much in progress

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of thoughts and replies on retrieval
+UserSchema.virtual('thoughtCount').get(function() {
+  return this.thoughts.reduce(
+    (total, thought) => total + thought.replies.length + 1,
+    0
+  );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
